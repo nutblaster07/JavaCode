eca53af4a375cfefb60931fc0f7da0c2efa59925
aroy7187 <aroy7187@gmail.com>
2026-03-27 20:41:16 +0530

    JavaCode

---
+++ Main.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args) {
+        
+            /*  ArrayList<String> list=new ArrayList<>();
+             list.add("hello");
+             list.add("1");
+             list.add("314");
+             Object o= list.get(1);
+             System.out.println(o);
+             System.out.println(list.get(2));*/
+           /*  Add<Integer> a=new Add<>();
+            a.setValue(1);
+            int b=a.getValue();
+            //System.out.println(b);
+            Box<Integer> box=new Box<>();
+            box.setValue(1);
+            int d=box.getValue();
+            System.out.println(d);
+            Pair<String,Integer> p=new Pair<>("Age",22);
+            System.out.println(p.getKey()+"="+p.getValue());
+            */
+
+         
+            Box<Double> box=new Box<>();
+            box.setValue(3.14);
+            Double str=box.getValue();
+            System.out.println(str);
+
+        
+        
+       
+
+        
+    }
+}
